82a5252c816a5c7f927ab5b19e0fe616a45caeb5
Phi Raien <kerala.emperor2@gmail.com>
2026-07-14 15:27:25 +0530

    remove back-to-top arrow from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,8 @@
-import { NavLink } from 'react-router-dom';
-
 export default function Footer() {
   return (
     <footer>
       <div className="container footer-inner">
         <p>&copy; 2026 Phi Raien.</p>
-        <NavLink to="/" className="back-to-top" aria-label="Back to top">&uarr;</NavLink>
       </div>
     </footer>
   );
