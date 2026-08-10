98c0e5556ed177d56f6ff8d79586a47cde756515
Phi Raien <kerala.emperor2@gmail.com>
2026-08-10 06:31:30 +0530

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-
-Portfolio Website
-
-- **React 19** + **Vite 6**
-- CSS3
-- Vanilla JS 
-
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <div className="container footer-inner">
-        <p>&copy; 2026 Rohan Joseph Sam.</p>
-      </div>
-    </footer>
-  );
-}
